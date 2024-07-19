264cd61680d008852f0757d5cd35efb8f6d04759
Rain Hu <intervalrain@gmail.com>
2024-07-19 23:25:44 +0800

    create config.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
+from src.config import settings
 
-app = FastAPI(title="DSM Bot")
+app = FastAPI(title=settings.PROJECT_NAME)
 
 @app.get("/")
-
 async def root():
-    return {"message": "Welcome to DSM Bot"}
+    return {"message": f"Welcome to {settings.PROJECT_NAME}"}
 
 if __name__ == "__main__":
     import uvicorn
